a632705c7d150e5b55f4a481875510f193f03469
blitheanon <blitheanon@qq.com>
2019-03-01 17:20:15 +0800

    Feature: 新增每次点击后单个点标记跳动，点击搜索列表项对应点标记跳动功能 Fix: 修复页面初次加载时加载两次bug index.html内移除onload方法

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 window.onload = function() {
     //Model层
     //5个地标
-    let Positions = [{
+    var Positions = [{
         position: new AMap.LngLat(106.674318, 26.619642), // 经纬度对象，也可以是经纬度构成的一维数组[116.39, 39.9]
         title: '贵阳北站'
     }, {
@@ -21,33 +21,13 @@ window.onload = function() {
     var markerPosition = function(position) {
         //封装地标
         //给观察对象提供地点名
-        this.positionName=ko.observable(position.title)
-        this.marker=ko.observable(new AMap.Marker(position))
+        this.positionName = ko.observable(position.title)
+        this.marker = ko.observable(new AMap.Marker(position))
     }
 
 
     //ViewModel层
     function ViewModel() {
-        let self=this
-        // this.positionList = ko.observableArray(Positions);
-        // 初始化一个空数组,catList为观察者
-        this.positionList = ko.observableArray([]);
-        //遍历所有猫，并添加到catList数组，给单个猫各类属性添加计算监控对象
-        Positions.forEach(function(item) {
-            //向catList观察者数组内添加对象
-            self.positionList.push(new markerPosition(item));
-        });
-
-        //列表点击监听,click对象参数默认为当前模型层对象,当前指的是搜索列表中的一个地点对象
-        this.positionItemClick = function(marker) {
-            console.log(marker);
-            //跳动当前地点
-            //设置点标记可点击
-            marker.setClickable(true);
-            //点击后标记跳动
-            marker.setAnimation('AMAP_ANIMATION_BOUNCE');
-        }
-
         // 创建地图对象
         var map = new AMap.Map('container', {
             mapStyle: 'amap://styles/whitesmoke', //设置地图的显示样式
@@ -59,12 +39,23 @@ window.onload = function() {
             map.addControl(new AMap.ToolBar());
         });
 
+        let self = this
+        // this.positionList = ko.observableArray(Positions);
+        // 初始化一个空数组,catList为观察者
+        this.positionList = ko.observableArray([]);
+        //遍历所有猫，并添加到catList数组，给单个猫各类属性添加计算监控对象
+        Positions.forEach(function(item) {
+            //向catList观察者数组内添加对象
+            self.positionList.push(new markerPosition(item));
+        });
+
         //遍历点标记添加事件
-        let positionList=this.positionList()
-        let markerList=[]
+        let positionList = this.positionList()
+        let markerList = []
         positionList.forEach(function(item) {
             // 取得地标对象内marker
-            let marker=item.marker()
+            let marker = item.marker()
+            // console.log(item.positionName());
             //生成单独的marker数组
             markerList.push(marker)
             //设置点标记的动画效果
@@ -73,14 +64,38 @@ window.onload = function() {
             marker.setClickable(true);
             //绑定点击事件
             marker.on('click', function() {
-                console.log('test');
-                //点击后标记跳动
+                // console.log(marker);
+                //移除其他跳动
+                self.removeAnimation();
+
+                //设置当前标记跳动
                 marker.setAnimation('AMAP_ANIMATION_BOUNCE');
             });
         });
-
         //将5个地标添加进地图
         map.add(markerList);
+
+
+        //列表点击监听,click对象参数默认为当前模型层对象,当前指的是搜索列表中的一个地点对象
+        this.listPositionClick = function(position) {
+            //移除其他跳动
+            self.removeAnimation();
+
+            let marker = position.marker()
+            // console.log(position.positionName());
+            //console.log(marker);
+            //跳动当前地点
+            marker.setMap(map);
+            // 设置点标记的动画效果，此处为弹跳效果
+            marker.setAnimation('AMAP_ANIMATION_BOUNCE');
+        }
+
+        //移除所有跳动
+        this.removeAnimation=function() {
+            positionList.forEach(function(item) {
+                item.marker().setAnimation('AMAP_ANIMATION_NONE')
+            })
+        }
     }
 
     // Activates knockout.js
