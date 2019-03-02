e3d24919b687fd7dea8a2106c200571d0919211e
blitheanon <blitheanon@qq.com>
2019-03-02 11:24:32 +0800

    Feature: 新增搜索结果能显示地图对应的点标记

--- js/main.js
+++ js/main.js
@@ -26,7 +26,6 @@ window.onload = function() {
         this.marker = ko.observable(new AMap.Marker(position))
     }
 
-
     //ViewModel层
     function ViewModel() {
         // 创建地图对象
@@ -52,34 +51,6 @@ window.onload = function() {
 
         //遍历点标记添加事件
         let positionList = this.positionList()
-        let markerList = []
-        positionList.forEach(function(item) {
-            // 取得地标对象内marker
-            let marker = item.marker()
-            // console.log(item.positionName());
-            //生成单独的marker数组
-            markerList.push(marker)
-            //设置点标记的动画效果
-            marker.setAnimation('AMAP_ANIMATION_DROP');
-            //设置点标记可点击
-            marker.setClickable(true);
-            //绑定点击事件
-            marker.on('click', function() {
-                // console.log(marker);
-                //移除其他跳动
-                self.removeAnimation();
-
-                //设置当前标记跳动
-                marker.setAnimation('AMAP_ANIMATION_BOUNCE');
-
-                //添加窗体信息
-                self.addInfoWindow(item)
-
-            });
-        });
-        //将5个地标添加进地图
-        map.add(markerList);
-
 
         //列表点击监听,click对象参数默认为当前模型层对象,当前指的是搜索列表中的一个地点对象
         this.listPositionClick = function(position) {
@@ -87,8 +58,6 @@ window.onload = function() {
             self.removeAnimation();
 
             let marker = position.marker()
-            // console.log(position.positionName());
-            //console.log(marker);
             //跳动当前地点
             marker.setMap(map);
             // 设置点标记的动画效果，此处为弹跳效果
@@ -97,6 +66,55 @@ window.onload = function() {
             //添加窗体信息
             self.addInfoWindow(position)
         }
+
+        //搜索关键字添加观察对象
+        this.query = ko.observable("")
+
+        this.filterMap = function(list) {
+            //使用搜素结果显示地图点标记
+            let markerList = []
+
+            list.forEach(function(item) {
+                // 取得地标对象内marker
+                let marker = item.marker()
+                //生成单独的marker数组
+                markerList.push(marker)
+                //设置点标记的动画效果
+                marker.setAnimation('AMAP_ANIMATION_DROP');
+                //设置点标记可点击
+                marker.setClickable(true);
+                //绑定点击事件
+                marker.on('click', function() {
+                    //移除其他跳动
+                    self.removeAnimation();
+
+                    //设置当前标记跳动
+                    marker.setAnimation('AMAP_ANIMATION_BOUNCE');
+
+                    //添加窗体信息
+                    self.addInfoWindow(item)
+
+                });
+            });
+
+            // 使用clearMap方法删除所有覆盖物
+            map.clearMap();
+            //将指定个地标添加进地图
+            map.add(markerList);
+        }
+
+        this.filteredPositionList = ko.computed(function() {
+            //通过输入关键字过滤地点list
+            let filteredPositionList = this.positionList().filter(function(item) {
+                let filterState = item.positionName().indexOf(self.query().trim()) != -1
+                return filterState;
+            })
+            //筛选地图点标记
+            this.filterMap(filteredPositionList)
+            //当无输入时，输入为空，indexOf返回值为0，filteredPositionList为所有地点
+            return filteredPositionList
+        }, this);
+
 
         //移除所有跳动
         this.removeAnimation = function() {
@@ -112,21 +130,9 @@ window.onload = function() {
                 offset: new AMap.Pixel(16, -45), //窗体位置的偏移量
                 content: item.positionName()
             });
-
             // 传入地标经纬度
             infoWindow.open(map, item.positionDetail())
         }
-
-        //搜索关键字添加观察对象
-        this.query = ko.observable("")
-
-        this.filteredPositionList = ko.computed(function() {
-            //通过输入关键字过滤地点list
-            return this.positionList().filter(function(item) {
-                return item.positionName().indexOf(self.query().trim())!=-1;
-            })
-        }, this);
-
     }
 
     // Activates knockout.js
